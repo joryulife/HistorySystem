966e88c95f3c5ef23bbdf2625498bde0211644f9
joryulife <joryulife@gmail.com>
2023-12-19 06:50:56 +0900

    cahnge db create

--- src/lib/ds.ts
+++ src/lib/ds.ts
@@ -8,21 +8,21 @@ const connection = mysql.createPool(dbConfig).promise();
 const createUserTable = `
     CREATE TABLE IF NOT EXISTS users (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(255) UNIQUE NOT NULL,
+        username VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL
-    )`;
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci`;
 
-    const createHistoryTable = `
+const createHistoryTable = `
     CREATE TABLE IF NOT EXISTS history (
         id INT AUTO_INCREMENT,
         userId INT,
-        nodeid VARCHAR(255),
+        nodeid VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
         height INT,
-        img VARCHAR(255),
+        img VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
         imgcreate BOOLEAN DEFAULT false,
-        parent VARCHAR(255),
-        title VARCHAR(255),
-        url VARCHAR(255),
+        parent VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+        title VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+        url VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
         width INT,
         created_at BIGINT,
         firstVisit DATETIME,
@@ -30,8 +30,9 @@ const createUserTable = `
         x INT,
         y INT,
         PRIMARY KEY(id),
-        UNIQUE(userId, nodeId)
-    )`;
+        UNIQUE(userId, nodeid)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci`;
+
 connection.query(createUserTable).then(([rows]) => {
     //console.log('Available Tables:', rows);
 }).catch(err => {
