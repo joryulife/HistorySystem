53abafc73e015043543bb7429a2e6f955c59c333
joryulife <joryulife@gmail.com>
2023-12-20 19:28:15 +0900

    W change

--- src/component/TreeComponents.tsx
+++ src/component/TreeComponents.tsx
@@ -164,7 +164,7 @@ export function TreeItemComponent({
 
     return (
         <TreeItemDynamic
-            W={(((node.date - minDate) / (maxDate - minDate) / 1.5) * windowWidth - parentpadding) > 0 
+            W={(((node.date - minDate) / (maxDate - minDate) / 1.5) * windowWidth - parentpadding) > 20 
             ? (((node.date - minDate) / (maxDate - minDate) / 1.5) * windowWidth - parentpadding) 
             : 0} // nullの場合は0を設定
             key={id}
